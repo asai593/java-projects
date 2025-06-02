c359fa24dd5fba3102dd325742566b8776e19ab4
asai593 <asaisumanth475@gmail.com>
2025-06-02 13:51:18 +0530

    Add files via upload

---
+++ swithchClass.java
@@ -0,0 +1,34 @@
+package xyt;
+import java.util.*;
+public class swithchClass {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		switch(s) {
+		case "monday":
+			System.out.print("let us do java");
+			break;
+		case "tuesday":
+			System.out.print("let us learn java");
+			break;
+		case "wendesday":
+			System.out.print("let us learn java");
+			break;
+		case "thursday":
+			System.out.print("let us learn java");
+			break;
+		case "friday":
+			System.out.print("let us learn java");
+			break;
+		case "saturday":
+			System.out.print("let us learn java");
+			break;
+		default:
+			System.out.println(" no case is there");
+			break;
+		}
+		
+	}
+}
+
+
